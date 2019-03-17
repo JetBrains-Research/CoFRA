ea24ce3a1245864064d96a69671ebaa3bbcdb1fc
Ilya Nozhkin <nozhkin.ii@gmail.com>
2019-03-17 16:57:34 +0300

    Fix paths in tests and plugin packing

--- run_tests.cmd
+++ run_tests.cmd
@@ -1,7 +1,5 @@
-Set RelativeOutputPath="bin/Release/net472"
-
 dotnet build test/src/PDASimulator.Tests/PDASimulator.Tests.csproj -c Release
 dotnet build test/src/ReSharperPlugin.Tests/ReSharperPlugin.Tests.csproj -c Release
 
-nunit3-console test/src/PDASimulator.Tests/%RelativeOutputPath%/PDASimulator.Tests.dll
-nunit3-console test/src/ReSharperPlugin.Tests/%RelativeOutputPath%/ReSharperPlugin.Tests.dll
+dotnet msbuild test/src/PDASimulator.Tests/PDASimulator.Tests.csproj -t:RunTests -p:Configuration=Release
+dotnet msbuild test/src/ReSharperPlugin.Tests/ReSharperPlugin.Tests.csproj -t:RunTests -p:Configuration=Release
--- run_tests_appveyor.cmd
+++ run_tests_appveyor.cmd
@@ -1,7 +1,5 @@
-Set RelativeOutputPath="bin/Release/net472"
-
 dotnet build test/src/PDASimulator.Tests/PDASimulator.Tests.csproj -c Release
 dotnet build test/src/ReSharperPlugin.Tests/ReSharperPlugin.Tests.csproj -c Release
 
-nunit3-console test/src/PDASimulator.Tests/%RelativeOutputPath%/PDASimulator.Tests.dll --result=PDASimulator.Tests.xml;format=AppVeyor
-nunit3-console test/src/ReSharperPlugin.Tests/%RelativeOutputPath%/ReSharperPlugin.Tests.dll --result=ReSharperPlugin.Tests.xml;format=AppVeyor
+dotnet msbuild test/src/PDASimulator.Tests/PDASimulator.Tests.csproj -t:RunTestsAppVeyor -p:Configuration=Release
+dotnet msbuild test/src/ReSharperPlugin.Tests/ReSharperPlugin.Tests.csproj -t:RunTestsAppVeyor -p:Configuration=Release
